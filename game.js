901e1e5008af77c51f2e7b60cdbf7288ceb73001
kunkyab <tenzinkunkyab5@gmail.com>
2025-05-21 23:07:36 -0400

    completed the simon game of color picking

--- game.js
+++ game.js
@@ -1,76 +1,124 @@
 var userClickedPattern = [];
 var gamePattern = [];
-var buttonColors= ["red", "blue", "green", "yellow"];
+var buttonColors = ["red", "blue", "green", "yellow"];
 
-function nextSequence() {
-  var randomNumber = Math.floor(Math.random()*4);
-  
-  var randomChosenColor = buttonColors[randomNumber];
+var started = false;
+var level = 0;
 
+var answer =  $("#level-2");
 
-gamePattern.push(randomChosenColor);
-
-var b = gamePattern[0];
-
-
-$("#"+b).fadeOut(1).fadeIn(100);
-playSound(b);
-
-//button thats cllick
-$(".btn").on("click",function (event) {
-  
-  $(event.target).fadeOut(1).fadeIn(100);
-  playSound(event.target.id);
-
-  //console.log(event.target.id);
-  animatePress(event.target.id);
-
- // console.log(event.currentTarget);
-  var result = handler(event.target.id);
-  userClickedPattern.push(result);
-//console.log(userClickedPattern);
+//when key is pressed
+$(document).keypress(function () {
+  if (!started)
+  {
+    //3. The h1 title starts out saying "Press A Key to Start", when the game has started, change this to say "Level 0".
+    $("#level-title").text("Level " + level);
+  nextSequence();
+  started = true;
+}
 });
 
+//button thats cllick
+$(".btn").click(function() {
+  //console.log(userClickedPattern);
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
 
+  playSound(userChosenColour);
 
+  animatePress(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);
 
+});
 
-function handler(userChosenColor) {
+//1. Create a new function called checkAnswer(), it should take one input with the name currentLevel
+function checkAnswer(currentLevel) {
+
+  //3. Write an if statement inside checkAnswer() to check if the most recent user answer is the same as the game pattern. If so then log "success", otherwise log "wrong".
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+
+   // console.log("success");
+
+    //4. If the user got the most recent answer right in step 3, then check that they have finished their sequence with another if statement.
+    if (userClickedPattern.length === gamePattern.length){
+
+      //5. Call nextSequence() after a 1000 millisecond delay.
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+
+    }
+
+  } else {
+
+     
+   // console.log("wrong");
+    function playWrongSound() {
+      var a = new Audio("sounds/wrong.mp3");
+      a.play();
+     
+    }
+    playWrongSound();
+
+    $("body").addClass("game-over");
+
+    setTimeout(() => {
+      $("body").removeClass("game-over");
+    }, 200);
+    var result = gamePattern.length;
+    
+    $("#level-title").text("Game Over,"+ "You got  " + result +  ", Press any key to restart");
+   
+
+     
+    startOver();
+   
+  }
+
+}
+
+function startOver() {
+
+  level= 0;
+  started = false;
+  gamePattern = [];
+
   
-  this.userChosenColor = userChosenColor;
- return userChosenColor;
+}
+
+
+
+function nextSequence() {
+
+    //6. Once nextSequence() is triggered, reset the userClickedPattern to an empty array ready for the next level.
+    userClickedPattern = [];
+  //4. Inside nextSequence(), increase the level by 1 every time nextSequence() is called.
+  level++;
+
+  //5. Inside nextSequence(), update the h1 with this change in the value of level.
+  $("#level-title").text("Level " + level);
+
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  var randomChosenColor = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeOut(100)
+    .fadeIn(100);
+  playSound(randomChosenColor);
 }
 
 function playSound(b) {
-  var a = new Audio("sounds\\"+b+".mp3");
-a.play();
+  var a = new Audio("sounds/" + b + ".mp3");
+  a.play();
 }
 
 function animatePress(currentColor) {
-  
- 
- 
-    $("#"+currentColor).addClass("pressed");
+  $("#" + currentColor).addClass("pressed");
 
-    setTimeout(() => {
-      $("#"+currentColor).removeClass("pressed");
-    }, 100);
-   // console.log("free tibet");
-  };
-
-
-
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+  // console.log("free tibet");
 }
-
-
-
-
-
-
-
-
-
-$(document).on("keydown",function (event){
- nextSequence();
-  })
-
